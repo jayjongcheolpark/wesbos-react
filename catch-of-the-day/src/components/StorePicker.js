9b83dba28e28ddf1a1c1feea61b430b767db5b51
gogolearn <jay.jongcheol.park@gmail.com>
2017-10-01 18:22:52 -0400

    Working with react events

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react'
 import { getFunName } from '../helpers'
 
 class StorePicker extends Component {
+  goToStore(event) {
+    event.preventDefault()
+    console.log(this.storeInput.value)
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+        <input
+          type="text"
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+          ref={(input) => { this.storeInput = input }}
+        />
         <button type="submit">Visit Store</button>
       </form>
     )
