25b761e8939fdfd7ce0fc0aab9464f2e252cfbd1
gogolearn <jay.jongcheol.park@gmail.com>
2017-10-01 21:56:10 -0400

    Updating order state

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -3,16 +3,18 @@ import { formatPrice } from '../helpers'
 
 class Fish extends Component {
   render() {
-    const { name, image, price, desc } = this.props.details
+    const { details, index } = this.props
+    const isAvailable = details.status === 'available'
+    const buttonText = isAvailable ? 'Add To Order' : 'Sold Out!'
     return (
       <li className="menu-fish">
-        <img src={image} alt={name} />
+        <img src={details.image} alt={name} />
         <h3 className="fish-name">
           {name}
-          <span className="price">{formatPrice(price)}</span>
+          <span className="price">{formatPrice(details.price)}</span>
         </h3>
-        <p>{desc}</p>
-        <button>Add To Order</button>
+        <p>{details.desc}</p>
+        <button onClick={() => this.props.addToOrder(index)} disabled={!isAvailable}>{buttonText}</button>
       </li>
     )
   }
